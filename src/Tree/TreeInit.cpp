ab4af9de9d65708c057f9e1a24c972dbf64374d1
kimdonghyun <167677958+nada-rlaehdgus-rokaf@users.noreply.github.com>
2025-03-22 11:52:41 +0000

    alive

---
+++ src/Tree/TreeInit.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+int dx[4] = {-1,1,0,0};
+int dy[4] = {0,0,1,-1};
+
+int main()
+{
+    
+
+    return 0;
+}
